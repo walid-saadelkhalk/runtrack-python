997bb31a05608bd530951e7402d67cfc934d1940
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2023-11-20 19:35:07 +0100

    devoir jour01

--- jour01/job11/third.py
+++ jour01/job11/third.py
@@ -7,3 +7,6 @@ print("alphabet dans le désordre", lettres)
 
 lettres.sort()
 print("alphabet dans l'ordre", lettres)
+
+alphabet_string = "".join(lettres)
+print("l'alphabet à nouveau dans une string:", alphabet_string)
